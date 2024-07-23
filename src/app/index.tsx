51327a6e95dc08534b17b01f62f35f5c9aa449bc
Julio Souza <julioamsf@gmail.com>
2024-07-23 16:30:50 -0400

    Add component Header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Teste 123.</Text>
+    <View>
+      <Text className="">Teste 123.</Text>
     </View>
   );
 }
