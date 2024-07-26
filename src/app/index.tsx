03209a843b68e862c889da808cfacbc27e6374d5
Julio Souza <julioamsf@gmail.com>
2024-07-26 12:04:12 -0400

    Add novos itens no header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,15 @@
-import { Text, View } from "react-native";
+import { View, ScrollView } from "react-native";
+import { Header } from "../components/header";
+import Constants from "expo-constants";
+
+const statusBarHeight = Constants.statusBarHeight;
 
 export default function Index() {
   return (
-    <View>
-      <Text className="">Teste 123.</Text>
-    </View>
+    <ScrollView className="bg-slate-200">
+      <View style={{ marginTop: statusBarHeight + 8 }} className="w-full px-4">
+        <Header />
+      </View>
+    </ScrollView>
   );
 }
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,23 @@
 import { View, Pressable, Text } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import { Feather, Ionicons } from "@expo/vector-icons";
 
 export function Header() {
   return (
-    <View>
-      <Pressable>
+    <View className="w-full flex flex-row items-center justify-between">
+      <Pressable className="w-10 h-10 bg-white rounded-full flex items-center justify-center">
         <Ionicons name="menu" size={20} color="#121212" />
+      </Pressable>
+
+      <View className="flex flex-col">
+        <Text>Localização</Text>
+        <View>
+          <Feather name="map-pin" size={14} color="#dd0000" />
+          <Text>Campo Grande - MS</Text>
+        </View>
+      </View>
+
+      <Pressable className="w-10 h-10 bg-white rounded-full flex items-center justify-center">
+        <Feather name="bell" size={20} color="#121212" />
       </Pressable>
     </View>
   );
